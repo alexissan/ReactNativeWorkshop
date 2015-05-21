5c941b8efad143dc653299a8b7dc2d3134a8b238
asantos <asantos@tuenti.com>
2015-05-21 22:19:28 +0200

    Finished AlbumsListScreen.js

--- AlbumsListScreen.js
+++ AlbumsListScreen.js
@@ -5,16 +5,42 @@ var AlbumDetailScreen = require('./AlbumDetailScreen');
 var React = require('react-native');
 
 var {
-  // You'll need to import a ListView here
-  View, 
+  ListView
 } = React;
 
 var AlbumsListScreen = React.createClass({
 
+  getInitialState() {
+    var dataSource = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+
+    return {
+      dataSource: dataSource.cloneWithRows(this.props.albums)
+    };
+  },
+
+  onRowPress(rowID) {
+    var album = this.props.albums[rowID];
+
+    this.props.navigator.push({
+      title: album.collectionName,
+      component: AlbumDetailScreen,
+      passProps: {album: album}
+    });
+  },
+
+  renderRow(rowData, sectionID, rowID) {
+    return (
+      <AlbumsRow
+        onPress={() => this.onRowPress(rowID)}
+        rowData={rowData} />
+    );
+  },
+
   render() {
     return (
-      // Replace this view by a ListView
-      <View />
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderRow} />
     );
   }
 });
